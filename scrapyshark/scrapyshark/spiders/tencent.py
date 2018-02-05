06670bee5ae9a5c5fdd2e766d5cb1b08e70941ec
dieice2030 <dieice2030@163.com>
2018-02-05 22:42:18 +0800

    using scrapy

--- scrapyshark/scrapyshark/spiders/tencent.py
+++ scrapyshark/scrapyshark/spiders/tencent.py
@@ -3,19 +3,20 @@ import scrapy
 
 
 class ScrapyTencent(scrapy.Spider):
-    name="tencent"
-    start_urls=[
-        'https://hr.tencent.com/position.php?&start='
-    ]
+    name = "tencent"
+    start_urls = ['https://hr.tencent.com/position.php?&start=']
 
-    def parse(self,response):
-        for quote in response.css("tr.even"):
-            yield{
-                'name':quote.css("td a").extract_first(),
-                'catalog':quote.css('td a').extract_first(),
-                'recruitNumber':quote.css('td a').extract_first()
+    def parse(self, response):
+        for quote in response.css("tr.even,tr.odd"):
+            yield {
+                'name': quote.css("td a::text").extract_first(),
+                'dataLink':"https://hr.tencent.com/"+quote.css("td a::attr(href)").extract_first(),
+                'catalog': quote.css("td")[1].css("::text").extract(),
+                'recruitNumber': quote.css("td")[2].css("::text").extract(),
+                'workLocation': quote.css("td")[3].css("::text").extract(),
+                'publishTime': quote.css("td")[4].css("::text").extract(),
             }
-        
-        next_page_url=response.css("a#next::attr(href)").extract_first()
+
+        next_page_url = response.css("a#next::attr(href)").extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
